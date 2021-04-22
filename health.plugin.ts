14f5153cf124609e475288ffd2cb58fe6db9ac07
Jonathan Massuchetti <jonathan.massuchetti@dappit.fr>
2021-04-22 20:00:25 +0200

    bump version

--- health.plugin.ts
+++ health.plugin.ts
@@ -13,7 +13,7 @@ export class HealthPlugin extends BasePlugin {
     static get info(){
         return {
             author: 'Jonathan MASSUCHETTI',
-            version: '1.0.2',
+            version: '1.0.3',
             name: 'lisk-plugin-health',
         };
     };
